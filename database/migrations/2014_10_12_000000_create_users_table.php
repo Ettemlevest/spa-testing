ac8bd7bb8a5eed11660d8a08fc9bbcd95a387eca
Imre Nagy <ettemlevest@outlook.com>
2018-06-09 19:29:11 +0200

    users table modification and db seeding added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,32 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('uid', 20)->unique();
+            $table->string('last_name');
+            $table->string('first_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('active')->default(1);
+            $table->string('last_seen_version', 20)->nullable();
+            $table->timestamp('last_login_at')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('modified_by')->nullable();
             $table->timestamps();
         });
+
+        // admin user
+        DB::table('users')->insert([
+            'id' => 100,
+            'uid' => 'admin',
+            'last_name' => 'IT Support',
+            'first_name' => '',
+            'email' => 'support@example.org',
+            'password' => Hash::make('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
     }
 
     /**
