957510e77a177f558072ca4db315299d1f7d9d31
Imre Nagy <ettemlevest@outlook.com>
2018-07-16 14:28:12 +0200

    OfficeUIFabric testing

--- resources/assets/js/app.ts
+++ resources/assets/js/app.ts
@@ -1,11 +1,13 @@
 
 import './bootstrap';
 import Vue from 'vue';
-import Vuetify from 'vuetify';
+// import Vuetify from 'vuetify';
+import OfficeUIFabricVue from 'office-ui-fabric-vue';
 
 (<any>window).Vue = require('vue');
 
-Vue.use(Vuetify);
+// Vue.use(Vuetify);
+Vue.use(OfficeUIFabricVue);
 
 const app = new Vue({
     el: '#app'
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
 
     <title>{{ config('app.name', 'SPA Skeleton') }}</title>
 
+    <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-js/1.2.0/css/fabric.min.css">
+    <link rel="stylesheet" href="https://static2.sharepointonline.com/files/fabric/office-ui-fabric-js/1.2.0/css/fabric.components.min.css">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
@@ -18,7 +21,48 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app">
+    <ou-command-bar>
+        <template slot='main'>
+            <ou-search-box type='commandBar' placeholder='Search' />
+            <ou-command-button icon='CircleRing'>Command</ou-command-button>
+            <ou-command-button icon='CircleRing'>Command</ou-command-button>
+            <ou-command-button icon='CircleRing'>Command</ou-command-button>
+        </template>
+        <template slot='side'>
+            <ou-command-button icon='CircleRing'>Command</ou-command-button>
+            <ou-contextual-menu>
+            <ou-command-button icon='Print' type='dropdown'>Print</ou-command-button>
+            <div slot='list'>
+                <ou-contextual-menu-item name='Animals' />
+                <ou-contextual-menu-item name='Books' />
+                <ou-contextual-menu-item name='Education' />
+                <ou-contextual-menu-item name='Music' />
+                <ou-contextual-menu-item name='Sports' disabled />
+            </div>
+            </ou-contextual-menu>
+            <ou-command-button icon='Add'>New</ou-command-button>
+        </template>
+    </ou-command-bar>
+
+
+        <ou-button>Create Account</ou-button>
+
+
+        <ou-list-item
+  primaryText='Alton Lafferty'
+  secondaryText='Meeting notes'
+  tertiaryText='Today we discussed the importance of a, b, and c in regards to d.'
+  metaText='2:42p'>
+  <ou-list-actions>
+    <ou-list-action-item icon='Mail'></ou-list-action-item>
+    <ou-list-action-item icon='Delete'></ou-list-action-item>
+    <ou-list-action-item icon='Flag'></ou-list-action-item>
+    <ou-list-action-item icon='Pinned'></ou-list-action-item>
+  </ou-list-actions>
+</ou-list-item>
+
+    </div>
 
     <!-- Scripts -->
     <script src="{{ mix('js/manifest.js') }}"></script>
