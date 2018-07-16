d49157116ad4e963bcf0490b474eacb07f92c489
Imre Nagy <ettemlevest@outlook.com>
2018-07-16 16:04:34 +0200

    users, parameters tables configuration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,37 +13,28 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cfg_users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('uid', 20)->unique();
             $table->string('last_name');
             $table->string('first_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->boolean('active')->default(1);
+            $table->integer('status_id')->unsigned()->default(1);
             $table->string('last_seen_version', 20)->nullable();
             $table->timestamp('last_login_at')->nullable();
             $table->timestamps();
         });
 
-        Schema::table('users', function (Blueprint $table) {
-            $table->integer('created_by')->nullable()->after('created_at');
-            $table->integer('updated_by')->nullable()->after('updated_at');
+        Schema::table('cfg_users', function (Blueprint $table) {
+            $table->integer('created_by')->unsigned()->nullable()->after('created_at');
+            $table->integer('updated_by')->unsigned()->nullable()->after('updated_at');
+
+            $table->foreign('created_by')->references('id')->on('cfg_users');
+            $table->foreign('updated_by')->references('id')->on('cfg_users');
         });
 
-        // admin user
-        DB::table('users')->insert([
-            'id' => 100,
-            'uid' => 'admin',
-            'last_name' => 'IT Support',
-            'first_name' => '',
-            'email' => 'support@example.org',
-            'password' => Hash::make('secret'),
-            'remember_token' => str_random(10),
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now()
-        ]);
+        $this->addAdminUser();
     }
 
     /**
@@ -53,6 +44,25 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cfg_users');
+    }
+
+    protected function addAdminUser()
+    {
+        $now = \Carbon\Carbon::now();
+        $admin_id = env('admin_id', 99);
+
+        DB::table('cfg_users')->insert([
+            'id' => env('admin_id', 99),
+            'last_name' => 'IT Support',
+            'first_name' => '',
+            'email' => 'support@example.org',
+            'password' => Hash::make('secret'),
+            'remember_token' => str_random(10),
+            'created_at' => $now,
+            'created_by' => $admin_id,
+            'updated_at' => $now,
+            'updated_by' => $admin_id
+        ]);
     }
 }
---
+++ database/migrations/2018_07_16_125603_parameters.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Parameters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cfg_parameters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category', 100);
+            $table->string('name');
+            $table->string('description');
+            $table->string('value', 500);
+            $table->string('default_value', 500);
+            $table->boolean('admin')->default(1);
+            $table->timestamps();
+        });
+
+        Schema::table('cfg_parameters', function (Blueprint $table) {
+            $table->integer('created_by')->unsigned()->nullable()->after('created_at');
+            $table->integer('updated_by')->unsigned()->nullable()->after('updated_at');
+
+            $table->foreign('created_by')->references('id')->on('cfg_users');
+            $table->foreign('updated_by')->references('id')->on('cfg_users');
+        });
+
+        $this->addDefaultParameters();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cfg_parameters');
+    }
+
+    /**
+     * Adding default parameters to the table.
+     *
+     * @return void
+     */
+    protected function addDefaultParameters()
+    {
+        $now = \Carbon\Carbon::now();
+        $admin_id = env('admin_id', 99);
+
+        // example:
+        // DB::table('cfg_parameters')->insert([
+        //     [
+        //         'category' => '',
+        //         'name' => '',
+        //         'description' => '',
+        //         'value' => '',
+        //         'default_value' => '',
+        //         'admin' => 0,
+        //         'created_at' => $now,
+        //         'created_by' => $admin_id,
+        //         'updated_at' => $now,
+        //         'updated_by' => $admin_id
+        //     ]
+        // ]);
+    }
+}
