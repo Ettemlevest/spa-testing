2f6e76af6584d746bcaa3f12ce0090c28b2c7e83
Imre Nagy <ettemlevest@outlook.com>
2018-07-17 22:11:29 +0200

    added Common\Models\Parameter

---
+++ app/Common/Models/Parameter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Common\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parameter extends Model
+{
+    /**
+     * Database table name for the model.
+     *
+     * @var string
+     */
+    protected $table = 'cfg_parameters';
+}
